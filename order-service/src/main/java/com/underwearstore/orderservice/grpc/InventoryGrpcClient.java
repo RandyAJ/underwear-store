0c897c9bcebd6266284d6db65791f09cb2a18e47
Asota Randy <randy201312@gmail.com>
2026-08-11 21:25:30 +0300

    order-service: создал класс-обертку для обращения к стабу grpc-сервиса

---
+++ order-service/src/main/java/com/underwearstore/orderservice/grpc/InventoryGrpcClient.java
@@ -0,0 +1,22 @@
+package com.underwearstore.orderservice.grpc;
+
+import org.springframework.stereotype.Service;
+import com.underwearstore.inventoryservice.grpc.InventoryServiceGrpc;
+import com.underwearstore.inventoryservice.grpc.ProductRequest;
+import com.underwearstore.inventoryservice.grpc.ProductResponse;
+
+@Service
+public class InventoryGrpcClient {
+
+    private final InventoryServiceGrpc.InventoryServiceBlockingStub stub;
+
+    public InventoryGrpcClient(InventoryServiceGrpc.InventoryServiceBlockingStub stub) {
+        this.stub = stub;
+    }
+
+    public ProductResponse checkAvailability(ProductRequest request){
+        System.out.println("ORDER InventoryGrpcClient: REQUEST = '" + request + "'");
+        return stub.checkAvailability(request);
+    }
+
+}
